c6a99ca599d85f39ff52a71ab823dc7485aaff49
Goarik Mkrtchyan <mkrtchyangoarik@gmail.com>
2024-08-09 22:12:01 +0300

    5

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework import filters, permissions, status, viewsets
+from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from posts.models import Comment, Group, Post
 
@@ -24,18 +24,12 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
-        return Comment.objects.filter(post_id=post_id)
-
-    def create(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return Response({
-                'detail': 'Authentication credentials were not provided.'},
-                status=status.HTTP_401_UNAUTHORIZED)
-        return super().create(request, *args, **kwargs)
+        post = get_object_or_404(Post, id=post_id)
+        return Comment.objects.filter(post_id=post.id)
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -45,4 +45,4 @@ class Follow(models.Model):
         ]
 
     def __str__(self):
-        return f"{self.user} follows {self.following}"
+        return f'{self.user} follows {self.following}'
